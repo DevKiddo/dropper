e70f9f89eda72a02c0719a789dd11dc4efd9eb51
Jimmy Gozal <jgozal@developerkit.co>
2016-10-11 00:03:02 -0400

    modules

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var http = require('http');
 
-http.createServer(function(req,res){
-    res.writeHead(200,{"Content-Type" : "text/plain"});
-    res.write("Hello World!");
-    res.end();
-}).listen(8080);
+function start() {
+
+    function onRequest(req, res) {
+        console.log("Request received")
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Hello World!");
+        res.end();
+    }
+
+    http.createServer(onRequest).listen(8080);
+    console.log('Server has started.');
+
+}
+
+exports.start = start;
