07b7a2bad16f5d30ef7e85187fa0b52df1f29174
Jimmy Gozal <jgozal@developerkit.co>
2016-10-15 09:50:33 -0400

    added post requests, showing server file system, file uploads pending

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
 
     function onRequest(req, res) {
+        var postData = "";
         var pathname = url.parse(req.url).pathname;
         console.log("Request for " + pathname + " received");
 
-        route(pathname);
+        req.setEncoding("utf8");
 
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.write("Hello World!");
-        res.end();
+        req.addListener('data', function (postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data for " + postDataChunk + " .")
+        });
+
+        req.addListener('end', function () {
+            route(handle, pathname, res, postData);
+        })
+
     }
 
     http.createServer(onRequest).listen(8080);
