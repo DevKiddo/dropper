1e92329609b7fb66dc36b21ffcd2e6b7bc793d68
Jimmy Gozal <jgozal@developerkit.co>
2016-10-16 01:12:00 -0400

    added upload handling

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,38 +1,46 @@
 var exec = require("child_process").exec;
-var querystring = require("querystring")
+var querystring = require("querystring");
+var formidable = require('formidable');
 
-function start(res, postData) {
+function start(res, req) {
     console.log("Request handler 'start' was called.");
 
-    var body = '<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
-        '</form>' +
-        '</body>' +
-        '</html>';
+    var body = '<form action="/upload" enctype="multipart/form-data" method="post">' +
+        '<input id="file" type="file" name="upload" multiple="multiple"><br>' +
+        '<input type="submit" value="Upload">' +
+        '</form>';
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(body);
 
-    exec('ls -lah', function (error, stdout, stderr) {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.write(stdout);
-        res.end();
-    })
-
-
 
 }
-function upload(res, postData) {
+function upload(res, req) {
     console.log("Request handler 'upload' was called.");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("You've sent: " + querystring.parse(postData).text);
-    res.end();
+
+    var form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
+    form.uploadDir = "./uploads/"; //set path
+    form.keepExtensions = true; //keep file original extensions
+
+    form
+
+        .on('fileBegin', function (name, file) {
+            //rename the incoming file to the file's name and handle empty uploads
+            file.path = form.uploadDir + (file.name ? file.name : 'error_no_upload.txt');
+        })
+
+        .parse(req, function (err, field, file) {
+            if (err) console.log(err);
+            exec('ls -sh uploads', function (error, stdout, stderr) {
+                res.writeHead(200, { 'content-type': 'text/plain' });
+                res.write(file.upload.name + (file.upload.name ? " was succesfully uploaded:\n\n" : "No file uploaded: make sure you choose a file!\n\n"));
+                res.write("Current state of directory:\n\n");
+                res.write(stdout);
+                res.end();
+            })
+
+        });
+
 }
 exports.start = start;
 exports.upload = upload;
